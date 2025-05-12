8f1e3999800d3682589e4db908569ea41d526bf9
Serhii <sergii.iefimov@gmail.com>
2025-05-12 20:05:18 +0300

    rca-2: * calendar component

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,39 +1,64 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
   Calendar as BigCalendar,
+  DateLocalizer,
   momentLocalizer,
   Views,
   type Event as CalendarEvent,
 } from 'react-big-calendar';
 import moment from 'moment';
+import './Calendar.scss';
+import { CustomToolbar } from '../Toolbar/CustomToolbar';
 
 const localizer = momentLocalizer(moment);
 
 const events: CalendarEvent[] = [
   {
-    title: 'Подія 1',
-    start: new Date(2025, 4, 10, 10, 0),
-    end: new Date(2025, 4, 10, 12, 0),
+    title: 'Tech interview',
+    start: new Date(2025, 4, 15, 10, 0),
+    end: new Date(2025, 4, 15, 12, 0),
   },
   {
-    title: 'Подія 2',
-    start: new Date(2025, 4, 11, 9, 0),
-    end: new Date(2025, 4, 11, 10, 0),
+    title: 'Big Day presentation',
+    start: new Date(2025, 4, 12, 9, 0),
+    end: new Date(2025, 4, 12, 10, 0),
   },
 ];
 
 export const Calendar: React.FC = () => {
+  const { components, formats } = useMemo(
+    () => ({
+      components: {
+        toolbar: CustomToolbar,
+      },
+      formats: {
+        dayHeaderFormat: (
+          date: Date,
+          culture?: string,
+          localizer?: DateLocalizer,
+        ) => (localizer ? localizer.format(date, 'dddd MMM D', culture) : ''),
+        dayRangeHeaderFormat: (
+          { start, end }: { start: Date; end: Date },
+          culture?: string,
+          localizer?: DateLocalizer,
+        ) =>
+          localizer
+            ? localizer.format(start, 'MMM D', culture) +
+              ' - ' +
+              localizer.format(end, 'MMM D', culture)
+            : '',
+      },
+    }),
+    [],
+  );
+
   return (
     <BigCalendar
+      defaultView={Views.MONTH}
       localizer={localizer}
       events={events}
-      startAccessor="start"
-      endAccessor="end"
-      defaultView={Views.MONTH}
-      defaultDate={new Date(2025, 4, 10)}
-      style={{ height: '100%' }}
+      formats={formats}
+      components={components}
     />
   );
 };
-
-
