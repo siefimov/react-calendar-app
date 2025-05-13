82c74173f9717b9977471eac8f0ac5c485fabf7b
Serhii <sergii.iefimov@gmail.com>
2025-05-13 10:41:52 +0300

    rca-2: + custom time gutter header

---
+++ src/components/CustomTimeGutterHeader/CustomTimeGutterHeader.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+import './CustomTimeGutterHeader.scss'
+
+export const CustomTimeGutterHeader: FC = () => (
+  <div className="custom-time-gutter-header">
+    <p></p>
+    <p>all day</p>
+  </div>
+);
