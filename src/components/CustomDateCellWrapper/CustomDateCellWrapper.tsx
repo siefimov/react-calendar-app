06fbe3ee21c387d00769369cd585e928cd63a69e
Serhii <sergii.iefimov@gmail.com>
2025-05-14 21:22:34 +0300

    rca-8: * fix cell border

--- src/components/CustomDateCellWrapper/CustomDateCellWrapper.tsx
+++ src/components/CustomDateCellWrapper/CustomDateCellWrapper.tsx
@@ -1,4 +1,5 @@
 import { useCalendarCellContext } from '../../context/CalendarCellContext';
+import './CustomDayCellWrapper.scss';
 
 export const CustomDateCellWrapper = ({ value, children }: any) => {
   const { selectedDate } = useCalendarCellContext();
@@ -11,9 +12,10 @@ export const CustomDateCellWrapper = ({ value, children }: any) => {
   return (
     <div
       data-date={value?.toISOString()}
+      className={isSelected ? 'custom-date-cell selected' : 'custom-date-cell'}
       style={{
         boxShadow: isSelected ? '0px 3px 6px #00000029' : undefined,
-        border: isSelected ? '1px solid #EAF0F4' : undefined,
+        border: isSelected ? '1px solid #EAF0F4' : undefined,        
         transition: 'box-shadow 0.2s',
         height: '100%',
         width: '100%',
